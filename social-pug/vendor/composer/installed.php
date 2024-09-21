cba88a78535ef43617a006b71f6719d7c02c6661
Sean Murphy <sean@iamseanmurphy.com>
2024-09-21 22:09:49 +0000

    1.34.3

---
+++ social-pug/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mediavine/mediavine-grow-pro',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f4569548f7c97255945d22ea5dac16fcead0c403',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'kernl/kernl-update-checker' => array(
+            'pretty_version' => '2.4.0',
+            'version' => '2.4.0.0',
+            'reference' => '84e4ec84f351bd766ae72cc1d9b27cf109382e95',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kernl/kernl-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mediavine/mediavine-grow-pro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f4569548f7c97255945d22ea5dac16fcead0c403',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
